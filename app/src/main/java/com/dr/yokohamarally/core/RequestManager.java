949fcfe73017f1e3e4b5e1fc4ddba3d8fb15e2c4
RyoBamboo <s175026@center.wakayama-u.ac.jp>
2014-12-29 18:45:40 +0900

    アプリ起動時に呼び出されるクラス作成

--- app/src/main/java/com/dr/yokohamarally/core/RequestManager.java
+++ app/src/main/java/com/dr/yokohamarally/core/RequestManager.java
@@ -1,4 +1,38 @@
 package com.dr.yokohamarally.core;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+// TODO: 画像系の処理が未実装。
+// TODO: 後ほど追加しておくこと。参考URL:https://github.com/stormzhang/AndroidVolley/blob/master/app/src/me/storm/volley/data/RequestManager.java
+
 public class RequestManager {
+    private static RequestQueue mRequestQueue;
+
+    public static void init(Context context) {
+        mRequestQueue = Volley.newRequestQueue(context);
+    }
+
+    public static RequestQueue getRequestQueue() {
+        if (mRequestQueue != null) {
+            return mRequestQueue;
+        } else {
+            throw new IllegalStateException("RequestQueue not initialized");
+        }
+    }
+
+    public static void addRequest(Request<?> request, Object tag) {
+        if (tag != null) {
+            request.setTag(tag);
+        }
+        mRequestQueue.add(request);
+    }
+
+    public static void cancelAll(Object tag) {
+        mRequestQueue.cancelAll(tag);
+    }
+
 }
