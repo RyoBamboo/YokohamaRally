8ca9d134e52a2850e3ff82cbc91462a1eef1fab9
moyoron64 <daix203440@gmail.com>
2015-02-01 09:28:07 +0900

    splash

--- app/src/main/java/com/dr/yokohamarally/activities/SplashActivity.java
+++ app/src/main/java/com/dr/yokohamarally/activities/SplashActivity.java
@@ -2,8 +2,10 @@ package com.dr.yokohamarally.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.view.Window;
 
 import com.dr.yokohamarally.R;
@@ -16,17 +18,28 @@ public class SplashActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
         Handler hdl = new Handler();
-        hdl.postDelayed(new splashHandler(), 500);
+        hdl.postDelayed(new splashHandler(), 900);
 
         
     }
 
     class splashHandler implements Runnable {
         public void run() {
-            Intent i = new Intent(getApplication(),
-                    MainActivity.class);
-            startActivity(i);
-            SplashActivity.this.finish();
+
+             /*------------------------
+         * ログインしているか確認する
+         * 現在は強制的にLoginActivityへ
+        /*----------------------*/
+            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+            if (LoginActiviry.isLogin(sp) == false) {
+                Intent intent = new Intent(getApplication(), LoginActiviry.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }else {
+                Intent i = new Intent(getApplication(), MainActivity.class);
+                startActivity(i);
+                SplashActivity.this.finish();
+            }
         }
     }
 }
